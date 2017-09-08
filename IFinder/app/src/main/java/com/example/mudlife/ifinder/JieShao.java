3c988a175ede1bde8c25e7aa24267c90c3d5074d
mudlife <mudlifez@wearlinks.com>
2017-09-08 20:35:26 +0800

    添加Fragment

---
+++ IFinder/app/src/main/java/com/example/mudlife/ifinder/JieShao.java
@@ -0,0 +1,21 @@
+package com.example.mudlife.ifinder;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by JX on 2017/9/8.
+ */
+
+public class JieShao extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.jieshao,container,false);
+        return view;
+    }
+}
--- IFinder/app/src/main/java/com/example/mudlife/ifinder/MainActivity.java
+++ IFinder/app/src/main/java/com/example/mudlife/ifinder/MainActivity.java
@@ -2,12 +2,91 @@ package com.example.mudlife.ifinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    RelativeLayout shouYeLayout = null;
+    RelativeLayout paiZhaoLayout = null;
+    RelativeLayout dingWeiLayout = null;
+    RelativeLayout jieShaoLayout = null;
+
+    ShouYe shouYeFragment = null;
+    PaiZhao paiZhaoFragment = null;
+    DingWei dingWeiFragment = null;
+    JieShao jieShaoFragment = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shouYeLayout = (RelativeLayout) findViewById(R.id.first_layout);
+        paiZhaoLayout = (RelativeLayout) findViewById(R.id.second_layout);
+        dingWeiLayout = (RelativeLayout) findViewById(R.id.third_layout);
+        jieShaoLayout = (RelativeLayout) findViewById(R.id.fourth_layout);
+
+
+
+        if(shouYeFragment == null){
+            shouYeFragment = new ShouYe();
+        }
+        getFragmentManager().beginTransaction().replace(R.id.bootmNav,shouYeFragment).commit();
+
+        /**
+         * 首页
+         */
+        shouYeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(shouYeFragment == null){
+                    shouYeFragment = new ShouYe();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.bootmNav,shouYeFragment).commit();
+            }
+        });
+
+        /**
+         * 拍照
+         */
+        paiZhaoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(paiZhaoFragment == null){
+                    paiZhaoFragment = new PaiZhao();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.bootmNav,paiZhaoFragment).commit();
+            }
+        });
+
+        /**
+         * 定位
+         */
+        dingWeiLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dingWeiFragment == null){
+                    dingWeiFragment = new DingWei();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.bootmNav,dingWeiFragment).commit();
+            }
+        });
+
+        /**
+         * 介绍
+         */
+        jieShaoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(jieShaoFragment == null){
+                    jieShaoFragment = new JieShao();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.bootmNav,jieShaoFragment).commit();
+            }
+        });
     }
 }
